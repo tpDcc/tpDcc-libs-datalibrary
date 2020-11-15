a70fed27aa7c1bf7935626ef703798db2753dba2
Tomas Poveda <tpovedatd@gmail.com>
2020-11-15 17:44:11 +0100

    Added module that contains constant definitions used by the library

---
+++ tpDcc/libs/datalibrary/core/consts.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains consts definitions used by data library
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from Qt.QtGui import QColor
+
+DEFAULT_LIBRARY_NAME = 'Default'
+DEFAULT_RECURSIVE_SEARCH_ENABLED = False
+DEFAULT_RECURSIVE_DEPTH = 8
+
+ITEM_DEFAULT_MAX_ICON_SIZE = 256
+ITEM_DEFAULT_FONT_SIZE = 12
+ITEM_DEFAULT_PLAYHEAD_COLOR = QColor(255, 255, 255, 220)
+ITEM_DEFAULT_THUMBNAIL_COLUMN = 0
+ITEM_DEFAULT_ENABLE_THUMBNAIL_THREAD = True
+
+GROUP_ITEM_DEFAULT_FONT_SIZE = 24
+GROUP_ITEM_PADDING_LEFT = 2
+GROUP_ITEM_PADDING_RIGHT = 20
+GROUP_ITEM_HEIGHT = 28
