47bd3bd21541d73b1a66c1819b6635e04861237e
Tomas Poveda <tpovedatd@gmail.com>
2020-11-15 17:44:29 +0100

    Added modules that contains exceptions and utils functions for the library

---
+++ tpDcc/libs/datalibrary/core/utils.py
@@ -0,0 +1,571 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains utils functions used by libraries
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import os
+import json
+import shutil
+import locale
+import logging
+import tempfile
+from collections import Mapping, OrderedDict
+
+from tpDcc.managers import configs
+from tpDcc.libs.python import osplatform, path as path_utils
+
+from tpDcc.libs.datalibrary.core import consts, exceptions
+
+LOGGER = logging.getLogger('tpDcc-libs-datalibrary')
+
+
+def default_library(settings_file_path=None):
+    """
+    Returns default library name
+    :return: str
+    """
+
+    libraries = read_settings(settings_file_path)
+
+    for name in libraries:
+        if libraries[name].get('default'):
+            return name
+
+    if consts.DEFAULT_LIBRARY_NAME in libraries:
+        return consts.DEFAULT_LIBRARY_NAME
+
+    for name in libraries:
+        return name
+
+    return consts.DEFAULT_LIBRARY_NAME
+
+
+def add_library(name, path, **settings):
+    """
+    Adds a new library with the given name, path and settings
+    :param name: str
+    :param path: str
+    :param settings: dict
+    """
+
+    libraries = read_settings(settings.pop('settings_file_path', None))
+    libraries.setdefault(name, dict())
+    libraries[name]['path'] = path
+    update(libraries[name], settings)
+    save_settings(libraries)
+
+
+def remove_library(name, settings_file_path=None):
+    """
+    Removes a library by name
+    :param name: str
+    :param settings_file_path: str or None
+    """
+
+    libraries = read_settings(settings_file_path)
+    if name in libraries:
+        del libraries[name]
+
+    save_settings(libraries)
+
+
+def settings_path():
+    """
+    Returns library settings file path
+    :return: str
+    """
+
+    datalib_config = configs.get_library_config('tpDcc-libs-datalibrary')
+    settings_file_path = datalib_config.get('settings_path')
+    settings_file_path = format_path(settings_file_path)
+
+    return settings_file_path
+
+
+def read_settings(settings_file_path):
+    """
+    Returns all user settings
+    :return: dict
+    """
+
+    path = settings_file_path or settings_path()
+    data = dict()
+    try:
+        data = read_json(path)
+    except Exception:
+        LOGGER.exception('Cannot read settings from "{}"'.format(path))
+
+    return data
+
+
+def update_settings(data, settings_file_path=None):
+    """
+    Updates the settings with the given data
+    :param data: dict
+    :param settings_file_path: str or None
+    """
+
+    settings = read_settings(settings_file_path)
+    update(settings, data)
+    save_settings(settings)
+
+
+def save_settings(data, settings_file_path=None):
+    """
+    Saves the given data to the settings path
+    :param data: dict
+    :param settings_file_path: str or None
+    """
+
+    path = settings_file_path or settings_path()
+    try:
+        save_json(path, data)
+    except Exception:
+        LOGGER.exception('Cannot save settings to "{}"'.format(path))
+
+
+def absolute_path(data, start):
+    """
+    Returns an absolute version of all the paths in data using the start path
+    :param data: str
+    :param start: str
+    :return: str
+    """
+
+    rel_path1 = path_utils.normalize_path(os.path.dirname(start))
+    rel_path2 = path_utils.normalize_path(os.path.dirname(rel_path1))
+    rel_path3 = path_utils.normalize_path(os.path.dirname(rel_path2))
+
+    if not rel_path1.endswith("/"):
+        rel_path1 += "/"
+
+    if not rel_path2.endswith("/"):
+        rel_path2 += "/"
+
+    if not rel_path3.endswith("/"):
+        rel_path3 += "/"
+
+    data = data.replace('../../../', rel_path3)
+    data = data.replace('../../', rel_path2)
+    data = data.replace('../', rel_path1)
+
+    return data
+
+
+def update(data, other):
+    """
+    Update teh value of a nested dictionary of varying depth
+    :param data: dict
+    :param other: dict
+    :return: dict
+    """
+
+    for key, value in other.items():
+        if isinstance(value, Mapping):
+            data[key] = update(data.get(key, {}), value)
+        else:
+            data[key] = value
+
+    return data
+
+
+def read(path):
+    """
+    Returns the contents of the given file
+    :param path: str
+    :return: str
+    """
+
+    data = ''
+    path = path_utils.normalize_path(path)
+    if os.path.isfile(path):
+        with open(path) as f:
+            data = f.read() or data
+    data = absolute_path(data, path)
+
+    return data
+
+
+def write(path, data):
+    """
+    Writes the given data to the given file on disk
+    :param path: str
+    :param data: str
+    """
+
+    path = path_utils.normalize_path(path)
+    data = path_utils.get_relative_path(data, path)
+
+    tmp = path + '.tmp'
+    bak = path + '.bak'
+
+    dirname = os.path.dirname(path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    if os.path.exists(tmp):
+        try:
+            os.remove(tmp)
+        except Exception:
+            pass
+        if os.path.exists(tmp):
+            msg = 'The path is locked for writing and cannot be accessed {}'.format(tmp)
+            raise IOError(msg)
+
+    try:
+        with open(tmp, 'w') as f:
+            f.write(data)
+            f.flush()
+
+        if os.path.exists(bak):
+            os.remove(bak)
+        if os.path.exists(path):
+            os.rename(path, bak)
+        if os.path.exists(tmp):
+            os.rename(tmp, path)
+        if os.path.exists(path) and os.path.exists(bak):
+            os.remove(bak)
+    except Exception:
+        if os.path.exists(tmp):
+            try:
+                os.remove(tmp)
+            except Exception:
+                pass
+        if not os.path.exists(path) and os.path.exists(bak):
+            os.rename(bak, path)
+
+        raise
+
+
+def update_json(path, data):
+    """
+    Update a JSON file with the given data
+    :param path: str
+    :param data: dict
+    """
+
+    data_ = read_json(path)
+    data_ = update(data_, data)
+    save_json(path, data_)
+
+
+def read_json(path):
+    """
+    Reads the given JSON file and deserialize it to a Python object
+    :param path: str
+    :return: dict
+    """
+
+    path = path_utils.normalize_path(path)
+    data = read(path) or '{}'
+    data = json.loads(data)
+
+    return data
+
+
+def save_json(path, data):
+    """
+    Serialize given tdata to a JSON string and write it to the given path
+    :param path: str
+    :param data: dict
+    """
+
+    path = path_utils.normalize_path(path)
+    data = OrderedDict(sorted(data.items(), key=lambda t: t[0]))
+    data = json.dumps(data, indent=4)
+    write(path, data)
+
+
+def replace_json(path, old, new, count=-1):
+    """
+    Replaces the old value with the new value in the given JSON file
+    :param path: str
+    :param old: str
+    :param new: str
+    :param count: int
+    :return: dict
+    """
+
+    old = old.encode("unicode_escape")
+    new = new.encode("unicode_escape")
+
+    data = read(path) or "{}"
+    data = data.replace(old, new, count)
+    data = json.loads(data)
+
+    save_json(path, data)
+
+    return data
+
+
+def format_path(format_string, path='', **kwargs):
+    """
+    Resolves given path by replacing necessary info with proper data
+    :param format_string: str
+    :param path: str
+    :param kwargs:
+    :return: str
+    """
+
+    dirname, name, extension = path_utils.split_path(path)
+    encoding = locale.getpreferredencoding()
+
+    temp = tempfile.gettempdir()
+    if temp:
+        try:
+            temp = temp.decode(encoding)
+        except Exception:
+            pass
+
+    username = osplatform.get_user(lower=True)
+    if username:
+        try:
+            username = username.decode(encoding)
+        except Exception:
+            pass
+
+    local = os.getenv('APPDATA') or os.getenv('HOME')
+    if local:
+        try:
+            local = local.decode(encoding)
+        except Exception:
+            pass
+
+    kwargs.update(os.environ)
+
+    format_dict = {
+        'name': name,
+        'path': path,
+        'root': path,
+        'user': username,
+        'temp': temp,
+        'home': local,
+        'local': local,
+        'dirname': dirname,
+        'extension': extension
+    }
+    kwargs.update(format_dict)
+
+    resolve_string = path_utils.normalize_path(format_string).format(**kwargs)
+
+    return path_utils.clean_path(resolve_string)
+
+
+def generate_unique_path(path, max_attempts=1000):
+    """
+    Generates a unique path on disk
+    :param path: str
+    :param max_attempts: int
+    :return: str
+    """
+
+    attempt = 1
+    dirname, name, extension = path_utils.split_path(path)
+    path_ = '{dirname}/{name} ({number}){extension}'
+
+    while os.path.exists(path):
+        attempt += 1
+        path = path_.format(name=name, number=attempt, dirname=dirname, extension=extension)
+        if attempt >= max_attempts:
+            raise ValueError('Cannot generate unique name for path {}'.format(path))
+
+    return path
+
+
+def rename_path_in_file(path, source, target):
+    """
+    Renames the given source path to the given target path
+    :param path: str
+    :param source: str
+    :param target: str
+    """
+
+    source = path_utils.normalize_path(source)
+    target = path_utils.normalize_path(target)
+
+    source1 = '"' + source + '"'
+    target1 = '"' + target + '"'
+
+    replace_json(path, source1, target1)
+
+    source2 = '"' + source
+    target2 = '"' + target
+
+    if not source2.endswith("/"):
+        source2 += "/"
+
+    if not target2.endswith("/"):
+        target2 += "/"
+
+    replace_json(path, source2, target2)
+
+
+def rename_path(source, target, extension=None, force=False):
+    """
+    Renames the given source path to the given destination path
+    :param source: str
+    :param target: str
+    :param extension: str
+    :param force: bool
+    :return: str
+    """
+
+    dirname = os.path.dirname(source)
+
+    if '/' not in target:
+        target = os.path.join(dirname, target)
+
+    if extension and extension not in target:
+        target += extension
+
+    source = path_utils.normalize_path(source)
+    target = path_utils.normalize_path(target)
+    LOGGER.debug('Renaming: {} > {}'.format(source, target))
+
+    if source == target and not force:
+        raise exceptions.RenamePathError('The source path and destination path are the same: {}'.format(source))
+    if os.path.exists(target) and not force:
+        raise exceptions.RenamePathError('Cannot save over an existing path: "{}"'.format(target))
+    if not os.path.exists(dirname):
+        raise exceptions.RenamePathError('The system cannot find the specified path: "{}"'.format(dirname))
+    if not os.path.exists(os.path.dirname(target)) and force:
+        os.mkdir(os.path.dirname(target))
+    if not os.path.exists(source):
+        raise exceptions.RenamePathError('The system cannot find the specified path: "{}"'.format(source))
+
+    os.rename(source, target)
+    LOGGER.debug('Renamed: {} > {}'.format(source, target))
+
+    return target
+
+
+def copy_path(source, target):
+    """
+    Makes a copy of the given source path to the given destination path
+    :param source: str
+    :param target: str
+    :return: str
+    """
+
+    dirname = os.path.dirname(source)
+    if '/' not in target:
+        target = os.path.join(dirname, target)
+
+    source = path_utils.normalize_path(source)
+    target = path_utils.normalize_path(target)
+
+    if source == target:
+        raise IOError('The source path and destination path are the same: {}'.format(source))
+    if os.path.exists(target):
+        raise IOError('Cannot copy over an existing path: {}'.format(target))
+    if os.path.isfile(source):
+        shutil.copy(source, target)
+    else:
+        shutil.copytree(source, target)
+
+    return target
+
+
+def move_path(source, target):
+    """
+    Moves the given source path to the given destination path
+    :param source: str
+    :param target: str
+    :return: str
+    """
+
+    source = path_utils.normalize_path(source)
+    dirname, name, extension = path_utils.split_path(source)
+    if not os.path.exists(source):
+        raise exceptions.MovePathError('No such file or directory: {}'.format(source))
+    if os.path.isdir(source):
+        target = '{}/{}{}'.format(target, name, extension)
+        target = generate_unique_path(target)
+    shutil.move(source, target)
+
+    return target
+
+
+def move_paths(source_paths, target):
+    """
+    Moves the given paths to the given destination paths
+    :param source_paths: list(str)
+    :param target: str
+    :return: list(str)
+    """
+
+    if not os.path.exists(target):
+        os.makedirs(target)
+
+    for source in source_paths or list():
+        if not source:
+            continue
+        basename = os.path.basename(source)
+        target_ = path_utils.normalize_path(os.path.join(target, basename))
+        LOGGER.debug('Moving Content: {} > {}'.format(source, target_))
+        shutil.move(source, target_)
+
+
+def remove_path(path):
+    """
+    Removes the given path from disk
+    :param path: str
+    """
+
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+def temp_path(*args):
+    """
+    Returns the temporal directory based on library settings
+    :param args:
+    """
+
+    datalib_config = configs.get_library_config('tpDcc-libs-datalibrary')
+    temp_path = datalib_config.get('temp_path')
+    temp_path = path_utils.normalize_path(os.path.join(format_path(temp_path), *args))
+
+    return temp_path
+
+
+def create_temp_path(name, clean=True, makedirs=True):
+    """
+    Creates a temporary directory with the given name
+    :param name: str
+    :param clean: bool
+    :param makedirs: bool
+    :return: bool
+    """
+
+    path = temp_path(name)
+
+    if clean and os.path.exists(path):
+        shutil.rmtree(path)
+
+    if makedirs and not os.path.exists(path):
+        os.makedirs(path)
+
+    return path
+
+
+def paths_from_urls(urls):
+    """
+    Returns the local file paths from the given QUrls
+    :param urls: list(QUrl)
+    :return: list(str)
+    """
+
+    for url in urls:
+        path = url.toLocalFile()
+        if osplatform.is_windows():
+            if path.startswith('/'):
+                path = path[1:]
+
+        yield path
